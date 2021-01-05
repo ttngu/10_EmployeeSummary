50b967126639c0e7f66b8b65aa8b4d9231c66d8c
ttngu <tammyn137@gmail.com>
2021-01-04 18:52:02 -0800

    added title

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,10 +2,11 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor(name, id, email, github){
+    constructor(name, id, email, github, title){
 
         super(name,id,email);
         this.github = github;
+        this.title = "Engineer";
     }
 
     // function to return fields
