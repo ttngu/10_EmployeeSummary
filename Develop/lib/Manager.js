71a713a2fb9c52e08815a8da58c1a09043a13986
ttngu <tammyn137@gmail.com>
2021-01-04 18:56:14 -0800

    updated title

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -2,11 +2,12 @@
 const Employee = require("./Employee");
 
 class Manager extends Employee {
-    constructor(name, id, email, officeNumber){
+    constructor(name, id, email, officeNumber, title){
 
         // validation, so parameters cannot be null
         super(name,id,email);
         this.officeNumber = officeNumber;
+        this.title = "Manager";
     }
 
     // function to return fields and role
@@ -14,7 +15,7 @@ class Manager extends Employee {
         return this.officeNumber;
     }
     getRole(){
-        return "Manager";
+        return this.title;
     }
 }
 
